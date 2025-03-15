21db54cef30829bb4a2378179dd63332766c87d2
Nagesh <chinagesh@gmail.com>
2025-03-15 17:03:10 -0400

    corrected the errors in lamdba-deploy

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from CICD github actions workflow vscode')
+        'body': json.dumps('Hello updated lambda')
     }
