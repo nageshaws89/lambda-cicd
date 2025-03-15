e24b2658af2fae68b1953fdd5c81901d656a149e
Nagesh <chinagesh@gmail.com>
2025-03-15 17:16:15 -0400

    updated the python version in lamdba-deploy

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello updated lambda with python version')
+        'body': json.dumps('Hello updated lambda with python version 3.13')
     }
